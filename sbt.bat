bbedb221b1693a614969f13283469ce3314929f4
Lars Reed <lars.reed@mesan.no>
2011-11-25 21:32:31 +0100

    REM path

--- sbt.bat
+++ sbt.bat
@@ -1,6 +1,6 @@
 @echo off
 
-PATH=c:\jdk6\bin;%PATH%
+rem PATH=c:\jdk6\bin;%PATH%
 
 set SCRIPT_DIR=%~dp0
 java -Xmx1024M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -jar "%SCRIPT_DIR%sbt-launch.jar" %*
